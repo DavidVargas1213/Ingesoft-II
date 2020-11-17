cc147fd6927a007e09ad4244d842ab3e94316315
DavidVargas1213 <47476556+DavidVargas1213@users.noreply.github.com>
2020-11-17 14:42:57 -0500

    Aprendo

---
+++ front-react-native/MathRude/assets/Screens/Aprendo.js
@@ -0,0 +1,140 @@
+import React, {Component} from 'react';
+//import {createStackNavigator} from 'react-navigation-stack';
+//import { createAppContainer } from 'react-navigation';
+import {ImageBackground, 
+        StyleSheet,
+        Text,
+        View, 
+        TouchableOpacity,
+      
+      } from 'react-native';
+
+export default class Repaso extends React.Component {
+    render(){
+      const{navigate}=this.props.navigation;
+      return(
+        <View  style={styles.container}>
+        <ImageBackground source={require('../imagenes/Titulo Aprendo.PNG')} style={{width: 350, height: 200, }}></ImageBackground>
+        <View style={styles.containerBottoms}>
+         
+          <View>
+           <TouchableOpacity onPress={() => navigate('RepasoSuma')}>
+             <Text style = {styles.bottom1}>
+                 SUMA         
+               </Text> 
+        </TouchableOpacity>
+          </View> 
+        <View>
+            <TouchableOpacity onPress={() => navigate('RepasoResta')}>
+              <Text style = {styles.bottom2}>
+                 
+                 RESTA
+                 
+                </Text> 
+            </TouchableOpacity>
+        </View> 
+        <View>
+            <TouchableOpacity onPress={() => navigate('RepasoMultiplicacion')}>
+              <Text style = {styles.bottom3}>
+                
+                MULTIPLICACION
+                
+                </Text> 
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity onPress={() => navigate('RepasoDivision')}>
+              <Text style = {styles.bottom4}>
+                
+                DIVISION
+                
+                </Text> 
+            </TouchableOpacity>
+        </View> 
+        </View>
+                
+               <ImageBackground source={require('../imagenes/BajaRepaso.jpeg')} style={styles.image2}></ImageBackground>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+    
+    container: {
+      flex: 1,
+      backgroundColor: '#fff', 
+      alignItems: 'center',
+      
+      backgroundColor:'white'
+    },
+    bottom1:{
+      fontFamily : 'Cochin',
+      paddingLeft:60,
+      paddingHorizontal:3,
+      paddingVertical:3,
+      marginBottom:10,
+      borderRadius:10,
+      borderWidth: 5,
+      borderColor: 'orange',
+      fontSize: 20,
+      fontWeight: "bold",
+      color: 'orange',
+      paddingBottom: '3',
+      backgroundColor: 'black'
+    },
+    bottom2:{
+      fontFamily : 'Cochin',
+      paddingLeft:60,
+      marginBottom:10,
+      borderRadius:10,
+      borderWidth: 5,
+      borderColor: 'orange',
+      fontSize: 20,
+      fontWeight: "bold",
+      color: 'orange',
+      justifyContent:'center',
+      alignItems: 'center',
+      backgroundColor: 'black'
+    },
+    bottom3:{
+      fontFamily : 'Cochin',
+      paddingHorizontal:3,
+      paddingVertical:3,
+      //marginHorizontal:20,
+      marginBottom:10,
+      borderRadius:10,
+      borderWidth: 5,
+      borderColor: 'orange',
+      fontSize: 20,
+      fontWeight: "bold",
+      color: 'orange',
+      paddingBottom: '3',
+      backgroundColor: 'black'
+    },
+    bottom4:{
+      fontFamily : 'Cochin',
+      paddingLeft:50,
+      paddingHorizontal:3,
+      paddingVertical:3,
+      borderRadius:10,
+      borderWidth: 5,
+      borderColor: 'orange',
+      fontSize: 20,
+      fontWeight: "bold",
+      color: 'orange',
+      paddingBottom: '3',
+      backgroundColor: 'black'
+    },
+    containerBottoms:{
+      //flexDirection: 'row', 
+      marginTop:'10%'
+    },
+    image2: {
+      flex:1,
+      marginLeft: 300,
+      paddingLeft: 290,
+      paddingTop: 100,
+      marginRight: '50%'
+    },
+  });
